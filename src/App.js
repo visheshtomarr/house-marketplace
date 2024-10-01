d84c8cd20d8421309c5e35d93beaa181284a7f14
visheshtomarr <visheshtomar7800@gmail.com>
2024-10-01 19:08:50 +0530

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
@@ -13,11 +14,12 @@ function App() {
         <Routes>
           <Route path="/" element={ <Explore /> } />
           <Route path="/offers" element={<Offers /> } />
-          <Route path="/profile" element={<Profile /> } />
+          <Route path="/profile" element={<SignIn /> } />
           <Route path="/sign-in" element={<SignIn /> } />
           <Route path="/sign-up" element={<SignUp /> } />
           <Route path="/forgot-password" element={<ForgotPassword /> } />
         </Routes>
+        <Navbar />
       </Router>
     </>
   );
