0a6415954a05ced6aff4a75c378e5c8056094fc8
visheshtomarr <visheshtomar7800@gmail.com>
2024-10-03 13:51:07 +0530

    Added react-toastify to SignIn and SignUp pages

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth/cordova";
 import { setDoc, doc, serverTimestamp } from "firebase/firestore";
@@ -68,7 +69,7 @@ function SignUp() {
             // Navigate to the Homepage, in our case, 'explore'.
             navigate("/");
         } catch (error) {
-            console.log(error);
+            toast.error('Something went wrong!');
         }
     }
 
