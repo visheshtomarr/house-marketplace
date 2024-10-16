630104827847292899548485a455265607b3e05d
visheshtomarr <visheshtomar7800@gmail.com>
2024-10-16 17:19:56 +0530

    Refactored code to upload images to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db } from "../firebase.config";  
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { v4 as uuidv4 } from "uuid";
 import Spinner from "../components/Spinner";
 
 function CreateListing() {
@@ -64,7 +67,7 @@ function CreateListing() {
     }, [isMounted])
 
     // Function to handle form submission.
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         setLoading(true);
@@ -92,6 +95,55 @@ function CreateListing() {
             location = address;
             console.log(geolocation, location);
         }
+
+        // Function to store images in firebase.
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage();
+                // To create a unique file name for every image.
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+                
+                const storageRef = ref(storage, 'images/' + fileName);
+                const uploadTask = uploadBytesResumable(storageRef, image);
+
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                            default:
+                                break
+                        }
+                    },
+                    (error) => {
+                        reject(error)
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL);
+                        });
+                    }
+                );
+            })
+        }
+
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false);
+            toast.error("Images not uploaded");
+            return;
+        })
+
+        console.log(imgUrls);
 
         setLoading(false);
     }
