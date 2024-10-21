f01edf37db4a2a8f46c35fb3bdde1b1052478eac
visheshtomarr <visheshtomar7800@gmail.com>
2024-10-21 20:47:16 +0530

    Added Listing page to fetch single listing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp /> } />
           <Route path="/forgot-password" element={<ForgotPassword /> } />
           <Route path="/create-listing" element={ <CreateListing /> } />
+          <Route path="/category/:categoryName/:listingId" element= { <Listing />} />
         </Routes>
         <Navbar />
       </Router>
